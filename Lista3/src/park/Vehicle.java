496d0198e9f78d394fd79f0a2e355a0afb6dbab0
hryniuklukas <hryniuklukas@gmail.com>
2022-06-29 14:22:24 +0200

    Vehicle -> Abstract

--- Lista3/src/park/Vehicle.java
+++ Lista3/src/park/Vehicle.java
@@ -1,8 +1,7 @@
 package park;
 
-public class Vehicle implements Identification {
+public abstract class Vehicle implements Identification {
     String numberPlates;
-    @Override
     public String returnNumberPlates() {
         return numberPlates;
     }
