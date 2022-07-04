6c2f26cac695c353657cd5ece595b31d23bc6844
hryniuklukas <hryniuklukas@gmail.com>
2022-07-04 19:10:47 +0200

    First stream tried

--- Streams/src/model/Main.java
+++ Streams/src/model/Main.java
@@ -1,7 +1,49 @@
 package model;
 
+import games.BoardGame;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Comparator;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        List<BoardGame> games = Arrays.asList(
+                new BoardGame("Terraforming Mars", 8.38, new BigDecimal("123.49"), 1, 5),
+                new BoardGame("Codenames", 7.82, new BigDecimal("64.95"), 2, 8),
+                new BoardGame("Puerto Rico", 8.07, new BigDecimal("149.99"), 2, 5),
+                new BoardGame("Terra Mystica", 8.26, new BigDecimal("252.99"), 2, 5),
+                new BoardGame("Scythe", 8.3, new BigDecimal("314.95"), 1, 5),
+                new BoardGame("Power Grid", 7.92, new BigDecimal("145"), 2, 6),
+                new BoardGame("7 Wonders Duel", 8.15, new BigDecimal("109.95"), 2, 2),
+                new BoardGame("Dominion: Intrigue", 7.77, new BigDecimal("159.95"), 2, 4),
+                new BoardGame("Patchwork", 7.77, new BigDecimal("75"), 2, 2),
+                new BoardGame("The Castles of Burgundy", 8.12, new BigDecimal("129.95"), 2, 4)
+        );
+
+        games.stream()
+                .filter(g -> g.maxPlayers > 4)
+                .filter(g -> g.rating > 8)
+                .filter(g -> new BigDecimal(150).compareTo(g.price) > 0)
+                .map(g -> g.name.toUpperCase())
+                .forEach(System.out::println);
+
+        double highestRanking = 0;
+        BoardGame bestGame = null;
+        for (BoardGame game : games) {
+            if (game.name.contains("a") && game.rating > highestRanking) {
+                    highestRanking = game.rating;
+                    bestGame = game;
+                }
+            }
+
+        System.out.println(bestGame.name);
+
+        BoardGame bestgame = games.stream()
+                .filter(g -> g.name.contains("a"))
+                .max(Comparator.comparingDouble(g -> g.rating)).get();
+        System.out.println(bestgame.name);
     }
 }
