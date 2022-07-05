60087f58ee97f0e4b1a8d5f285d8bf1399352b81
hryniuklukas <hryniuklukas@gmail.com>
2022-07-05 16:03:09 +0200

    Mapper converted to single stream operation

--- Streams/src/streamTask/Mapper.java
+++ Streams/src/streamTask/Mapper.java
@@ -2,27 +2,18 @@ package streamTask;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 public interface Mapper {
-    static List<Target> mapSourceToTarget(List<Source> sourceList)
-    {
+    static List<Target> mapSourceToTarget(List<Source> sourceList) {
         List<Target> targetList = new ArrayList<>();
 
+        sourceList.stream().collect(Collectors.groupingBy(Source::getTypeAndSomeId)).forEach((key, value) -> {
+            Target buffer = new Target(value.get(0).getType(), value.get(0).getSomeId());
+            value.forEach(s -> buffer.addIdentifier(s.getIdentifier()));
+            targetList.add(buffer);
+        });
 
-        Map<String,List<Source>> thirdMap = sourceList.stream().collect(Collectors.groupingBy(Source::getTypeAndSomeId));
-        System.out.println(thirdMap);
-        int i = 0;
-        for (String key:thirdMap.keySet()
-        ) {
-            targetList.add(new Target(thirdMap.get(key).get(0).getType(), thirdMap.get(key).get(0).getSomeId()));
-            for (Source source:thirdMap.get(key)
-            ) {
-                targetList.get(i).addIdentifier(source.getIdentifier());
-            }
-        i++;
-        }
         return targetList;
     }
 }
